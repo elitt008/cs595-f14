c0f7efbe64e1674ead85623b119897a554e4b40c
elitt008 <elitt008@odu.edu>
2014-10-31 17:05:29 -0400

    updated code

--- assignment6/igraph/graphing.py
+++ assignment6/igraph/graphing.py
@@ -3,51 +3,89 @@ from igraph import *
 
 karate = Graph.Read_GraphML("karate.GraphML")
 summary(karate)
-print karate.vs
-layout=karate.layout_kamada_kawai()
-plot(karate,layout=layout)
-#for element in karate.vs["name"]:
-#   print element
-#for element in karate.vs["Faction"]:
-#   print element
-print type(karate.es["weight"])
-#count=0
-#for element in karate.es["weight"]:
-#   print count," ",element
-#   count=count+1
-#count=1
-edgebetweenesslist = karate.edge_betweenness()
-#for element in betweenesslist:
-#   print count," ", element
-#   count = count + 1
 
-#vertexbs=karate.community_edge_betweenness(weights=karate.es["weight"]).as_clustering(n=2)
-#for element in vertexbs:
-#   print type(element)
-#   count=1
-#   for element1 in element:
-#      print count," ",element1
-#      count = count + 1 
 count=1
-bs=karate.community_leading_eigenvector(clusters=2,weights=karate.es["weight"])
+list1=[]
+for name in karate.vs["name"]:
+   if "Actor" in name:
+      list1.append(str(count))
+      #print name
+      #print karate.vs["label"][count]
+   else: 
+      list1.append(name)
+   count = count + 1
+#print list1
+karate.vs["label"]=list1
+#print karate.vs["label"]
 
+bs2=karate.community_leading_eigenvector(clusters=2,weights=karate.es["weight"])
+bs3=karate.community_leading_eigenvector(clusters=3,weights=karate.es["weight"])
+bs4=karate.community_leading_eigenvector(clusters=5,weights=karate.es["weight"])
+bs5=karate.community_leading_eigenvector(clusters=7,weights=karate.es["weight"])
 
-for element1 in bs:
-   summary(element1)
-#   for element2 in element1:
+summary(bs2)
+summary(bs3)
+summary(bs4)
+summary(bs5)
+
+layout=karate.layout_kamada_kawai()
+#layout=karate.layout_circle()
+#layout=karate.layout_drl()
+#layout=karate.layout_fruchterman_reingold
+
+colorlist2=[]
+colorlist3=[]
+colorlist4=[]
+colorlist5=[]
+
+for i in range (0,34):
+   colorlist2.append("blue")
+   colorlist3.append("blue")
+   colorlist4.append("blue")
+   colorlist5.append("blue")
+
+for i in range (0,34):
+   if i in bs2[0]:
+      colorlist2[i]="blue"
+   elif i in bs2[1]:
+      colorlist2[i]="red"
+
+for i in range (0,34):
+   if i in bs3[0]:
+      colorlist3[i]="blue"
+   elif i in bs3[1]:
+      colorlist3[i]="red"
+   elif i in bs3[2]:
+      colorlist3[i]="green"
       
+for i in range (0,34):
+   if i in bs4[0]:
+      colorlist4[i]="blue"
+   elif i in bs4[1]:
+      colorlist4[i]="red"
+   elif i in bs4[2]:
+      colorlist4[i]="green"
+   elif i in bs4[3]:
+      colorlist4[i]="orange"
 
-   print count," ",element1
-   count = count + 1 
+for i in range (0,34):
+   if i in bs5[0]:
+      colorlist5[i]="blue"
+   elif i in bs5[1]:
+      colorlist5[i]="red"
+   elif i in bs5[2]:
+      colorlist5[i]="green"
+   elif i in bs5[3]:
+      colorlist5[i]="orange"
+   elif i in bs5[4]:
+      colorlist5[i]="pink"
 
+print bs2
+print bs3
+print bs4
+print bs5
 
-
-#g=bs.cluster_graph()
-#layout=g.layout_kamada_kawai()
-#plot(g,layout=layout)
-#print karate.community_edge_betweenness(directed=False)
-#print karate.community_edge_betweenness(directed=False,clusters=5)
-#print karate.community_edge_betweenness(directed=False,clusters=5,weights=karate.es["weight"])
-#print karate.community_edge_betweenness(directed=False)
-#print karate.community_edge_betweenness(directed=False,clusters=5)
-#print karate.community_edge_betweenness(directed=False,clusters=5,weights=karate.es["weight"])
+plot(karate,layout=layout,vertex_color=colorlist2)
+plot(karate,layout=layout,vertex_color=colorlist3)
+plot(karate,layout=layout,vertex_color=colorlist4)
+plot(karate,layout=layout,vertex_color=colorlist5)
